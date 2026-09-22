3e00ed6e17b4f41177ab2c1cb8239f7216df8b09
Shankar00-M <digitalmcalls.official@gmail.com>
2026-09-22 23:31:51 +0530

    upadte url

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://foygjkjrmnofenscwwam.supabase.co"
+const supabaseAnonKey = "sb_publishable_vQ_bBWfbv0496p-EyppM5Q__RxVA0MM"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
